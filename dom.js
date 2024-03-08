8f375aa2bd62323e8c03280d5584297a2c3ea3de
AndreHLudwig <andre.udesc@hotmail.com>
2024-03-08 16:48:12 -0300

    Update dom.js
    08/03/2024

--- dom.js
+++ dom.js
@@ -57,3 +57,87 @@ elemento3.style.backgroundColor = "silver";
 //background-color => babackgroundColor
 
 //Navegação entre nós
+const elemento4 = document.querySelector("#meuInput");
+
+// Obtendo o elemento pai
+const pai = elemento4.parentNode;
+
+console.log(pai);
+
+//obter o primeiro filho
+const primeiroFilho = pai.firstChild;
+
+console.log(primeiroFilho);
+
+const ultimoFilho = pai.lastChild;
+
+console.log(ultimoFilho);
+
+//Manipulação de estrutura do DOM
+const novoElemento = document.createElement("div");
+
+console.log(novoElemento);
+
+novoElemento.textContent = "Minha div de JavaScript";
+
+console.log(novoElemento);
+
+//Adicionar elemento via document
+document.body.appendChild(novoElemento);
+document.body.insertBefore(novoElemento, pai);
+
+document.body.removeChild(elmentoPorId);
+
+//Eventos
+const botao = document.querySelector("button");
+
+botao.addEventListener("click", function(){
+    console.log("Botão clicado!");
+});
+
+//Mouse
+const elemento5 = document.querySelector("#meuFormulario");
+
+elemento5.addEventListener("mouseover", function(){
+    console.log("O mouse passou aqui!");
+});
+
+const campoInput = document.querySelector("#meuInput");
+
+campoInput.addEventListener("keydown", function(){
+    console.log("Tecla pressionada!");
+});
+
+//formulario
+
+const form = document.querySelector("form");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    
+    console.log("Formulário enviado!");
+});
+
+//propagação de eventos
+document.querySelector("#elementoPai").addEventListener("click", function (){
+    console.log("Clique capturado no pai");
+});
+
+document.querySelector("#elementoFilho").addEventListener("click", function (event){
+    //event.stopPropagation(); //para a propagação de eventos
+
+    console.log("Elemento Filho");
+});
+
+document.querySelector("#meuLink").addEventListener("click", function(event){
+    event.preventDefault();
+
+    console.log("Clicou no link!");
+});
+
+//delegação de eventos
+document.querySelector("#elementoPai").addEventListener("click", function(){
+    if(event.target.matches(".classeDosFilhos")){
+        console.log("Evento delegado para o filho!")
+    }
+});
